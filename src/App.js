b8d687d719c0dae8a7875f3043f63fd330d4e30d
UsernameOfAlexT <jjcctamale@gmail.com>
2020-04-19 17:09:19 -0400

    Skeleton of a silly ability gen idea I had

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// TODO as the structure comes into place, start breaking things off
+
+const POSSIBLE_TARGETS = [
+  "allies",
+  "enemies",
+  "all units"
+];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="backbox">
+      <div id="topbar">Random Ability Generator</div>
+          <AbilityTemplate/>
     </div>
   );
+}
+
+class AbilityTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {templateId: 0};
+    this.pickRandomTemplate = this.pickRandomTemplate.bind(this);
+  }
+
+  componentDidMount() {
+    this.pickRandomTemplate();
+  }
+
+  pickRandomTemplate() {
+    // TODO meaningless for now, use length when there is a real list to choose from
+    const randomTemplate = randomInt(10);
+    this.setState({
+      templateId : randomTemplate
+    });
+  }
+
+  render() {
+    // TODO cond render when we have multiple templates
+    let generatedFragment = <RandomTargetType/>;
+    return (
+      <div id="content-main">
+        <div className="text-content">
+          {generatedFragment}
+          <p>Template #: {this.state.templateId}</p>
+        </div>
+        <button className="btn" onClick={this.pickRandomTemplate}>Another One!</button>
+      </div>
+    )
+  }
+}
+
+function RandomTargetType() {
+  const maxTargetsPossible = POSSIBLE_TARGETS.length;
+  const targetInd = randomInt(maxTargetsPossible);
+
+  return (
+    <TargetType selectionIndex={targetInd}/>
+  );
+}
+
+function TargetType(props) {
+  const selectionIndex = props.selectionIndex;
+
+  return (
+    <div>
+      {selectionIndex < POSSIBLE_TARGETS.length 
+      ? POSSIBLE_TARGETS[selectionIndex]
+      : '???'
+      }
+    </div>
+  );
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 export default App;
